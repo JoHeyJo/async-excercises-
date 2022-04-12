d739501d21ac208a1034dfa9e760f79702424822
jf <jpf0628@gmail.com>
2022-04-11 18:23:52 -0700

    WIP: Deck of cards. Part 2

--- cards/app.js
+++ cards/app.js
@@ -7,36 +7,24 @@ let DECK_ID = async function getDeckId(){
     let deckData = await axios({ url:`${BASE_URL}/deck/new/shuffle/?deck_count=1`});
     DECK_ID = deckData.data.deck_id;
 }
-async function randomCard(numOfDraws) {
+async function randomCard(DECK_ID) {
 //   evt.preventDefault()
 //   $("#lucky-number").empty()
 //   console.log("Successfully prevented default event")
 //   const favoriteNum = $(".number").val()
-    let hand = []
-    const deckId = await axios({ url:`${BASE_URL}/deck/new/shuffle/?deck_count=1`});
-    // const drawOne = await axios({ url:`${BASE_URL}/deck/${deckId.data.deck_id}/draw/?count=1`}); = 5S
+    let cardResp = axios({ url:`${BASE_URL}/deck/${DECK_ID}/draw/?count=1`})
 
-    for(let i = 0; i < numOfDraws; i++){
-        hand.push(await axios({ url:`${BASE_URL}/deck/${deckId.data.deck_id}/draw/?count=1`}))
-    }
-    console.log(hand)
-    // console.log(drawOneAgain)
-
-//   const response = Promise.all([resp1, resp2, resp3, resp4])
-//   response
-//   .then((allResp)=>{
-//     let responseText = allResp.map(respObject=>{
-      
-//       return respObject.data.text
-//     })
-//     showResults(responseText);
-//   })
-//   .catch((err) => {
-//     populateError(err)
-//   })
+    cardResp 
+  .then((cardResp)=>{
+    console.log( cardResp.data.cards[0].image)
+    console.log( cardResp.data.cards[0].code)
+      return cardResp.data.cards[0].image
+  })
+  .catch((err) => {
+    populateError(err)
+  })
 }
 
-// event listener
 
 // $("#lucky-number").on("submit", luckyNums);
 
@@ -45,7 +33,7 @@ async function randomCard(numOfDraws) {
 // function showResults(array) {
 //   console.log("populating Results")
 //   console.log(array)
-//   $("#lucky-number").append("<hr>Here Are the facts about your favorite number: ")
+//   $("#cards").append("<hr>Here Are the facts about your favorite number: ")
 //   const $facts = $(".facts");
 //   for(let text of array){
 //   $facts.append(`<li>${text}</li>`)
@@ -55,4 +43,4 @@ async function randomCard(numOfDraws) {
 // function populateError(err){
 //   $("#lucky-number").append(`${err}`)
 // }
-//createand populate error element
+DECK_ID();
